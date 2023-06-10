14e4b937f14071d77d71ef2b94b4e48c0af05e17
ycheng <36308352+ycheng22@users.noreply.github.com>
2023-06-10 13:05:34 -0400

    ch3 db context

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -10,6 +10,9 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddControllers();
 
