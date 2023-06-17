7f241026021e332dbd53b4cd98b59da1783df808
ycheng <36308352+ycheng22@users.noreply.github.com>
2023-06-17 12:59:26 -0400

    ch16 Paging, update GetEmployeesAsync in Repository, add X-Pagination in ConfigureCors

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -15,11 +15,15 @@ namespace Repository
             EmployeeParameters employeeParameters, bool trackChanges)
         {
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-                            .OrderBy(e => e.Name)
-                            .ToListAsync();
+                .OrderBy(e => e.Name)
+                .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+                .Take(employeeParameters.PageSize)
+                .ToListAsync();
 
-            return PagedList<Employee>
-                .ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
+            var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
+
+            return new PagedList<Employee>(employees, count,
+                employeeParameters.PageNumber, employeeParameters.PageSize);
         }
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
